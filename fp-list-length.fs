12dd980123af03c11aeca49ed5513ccc000d2c75
Dara <dara@Mac-de-Dara.local>
2016-10-09 03:17:31 +0200

    Add FP List Length

---
+++ fp-list-length.fs
@@ -0,0 +1,13 @@
+open System
+
+let read_and_parse()=
+    let mutable arr = []
+    let mutable buff = Console.ReadLine()
+    while buff <> null do
+            arr <- Int32.Parse(buff)::arr
+            buff <- Console.ReadLine()
+    arr
+
+let main =
+    let arr = read_and_parse()
+    printf "%A" <| arr.Length
