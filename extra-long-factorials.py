37cf4c873d3e225eb59863c932abb97eb4780a9a
Dara <topen.pech.s@gmail.com>
2016-11-26 02:32:08 +0100

    Add Extra Long Factorials

---
+++ extra-long-factorials.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+import sys
+
+
+def factorial(n):
+    if n < 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+n = int(raw_input().strip())
+print factorial(n)
